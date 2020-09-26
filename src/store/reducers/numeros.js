187d5b33ac61d1d82f6448a4b1191255a38bd7c6
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-09-26 20:01:48 -0300

    Refactor Reducer

---
+++ src/store/reducers/numeros.js
@@ -0,0 +1,21 @@
+const initialState = {
+    min: 1,
+    max: 10
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+        case 'NUM_MINIMO_ALTERADO':
+            return{
+                ...state,
+                min: action.payload
+            }
+        case 'NUM_MAXIMO_ALTERADO':
+            return{
+                ...state,
+                max: action.payload
+            }
+        default:
+            return state;
+    }
+}
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,34 +1,9 @@
 import {createStore, combineReducers} from 'redux';
 
+import numerosReducer from './reducers/numeros';
+
 const reducers = combineReducers({
-    numeros: function(state, action){
-        switch(action.type){
-            case 'NUM_MINIMO_ALTERADO':
-                return{
-                    ...state,
-                    min: action.payload
-                }
-            case 'NUM_MAXIMO_ALTERADO':
-                return{
-                    ...state,
-                    max: action.payload
-                }
-            default:
-                return {
-                    min: 7,
-                    max: 31
-                }
-        }
-    },
-    nomes: function(state, action){
-        console.log('Reducer de nomes chamado...')
-        console.log(state, ' ', action)
-        return[
-            'Ana', 
-            'Bia', 
-            'Carlos'
-        ]
-    }
+    numeros: numerosReducer
 })
 
 function storeConfig(){
