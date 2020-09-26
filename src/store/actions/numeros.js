11da5fb6c1ad6c616eef3bea2d48cc3d8ea653c2
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-09-26 20:07:09 -0300

    Create constants for the types of actions

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,15 +1,16 @@
+import {NUM_MINIMO_ALTERADO, NUM_MAXIMO_ALTERADO} from './actionTypes';
 
 //action creator
 export function alterarNumeroMinimo(novoNumero){
     return{
-        type: 'NUM_MINIMO_ALTERADO',
+        type: NUM_MINIMO_ALTERADO,
         payload: novoNumero
     }
 }
 
 export function alterarNumeroMaximo(novoNumero){
     return{
-        type: 'NUM_MAXIMO_ALTERADO',
+        type: NUM_MAXIMO_ALTERADO,
         payload: novoNumero
     }
 }
--- src/store/reducers/numeros.js
+++ src/store/reducers/numeros.js
@@ -1,3 +1,6 @@
+import {NUM_MINIMO_ALTERADO, NUM_MAXIMO_ALTERADO} from '../actions/actionTypes';
+
+
 const initialState = {
     min: 1,
     max: 10
@@ -5,12 +8,12 @@ const initialState = {
 
 export default function(state = initialState, action){
     switch(action.type){
-        case 'NUM_MINIMO_ALTERADO':
+        case NUM_MINIMO_ALTERADO:
             return{
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAXIMO_ALTERADO':
+        case NUM_MAXIMO_ALTERADO:
             return{
                 ...state,
                 max: action.payload
